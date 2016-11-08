3b9cdb34688079409d183901fb080607e48a09e8
Derick Quartey <niikwartey1@gmail.com>
2016-11-08 15:58:19 -0500

    initial commit- skeleton app

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,7 @@
+class ProfileController < ApplicationController
+  def dairy
+  end
+
+  def settings
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'profile#dairy'
+
+  get 'profile/settings'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
